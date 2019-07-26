3158f8758a33d0dbd7b6c700019343d771da7b39
casmer <caseyg@lalosoft.com>
2019-07-26 10:25:21 -0400

    adding joy stick listener

--- robot_control_host/robot_control_host.cpp
+++ robot_control_host/robot_control_host.cpp
@@ -17,21 +17,29 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/joy.hpp"
 
+
+using std::placeholders::_1;
 using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
  * member function as a callback from the timer. */
 
-class MinimalPublisher : public rclcpp::Node
+class RobotControlHost : public rclcpp::Node
 {
 public:
-  MinimalPublisher()
-  : Node("minimal_publisher"), count_(0)
+	RobotControlHost()
+  : Node("RobotControlHost"), count_(0)
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    publisher_ = this->create_publisher<std_msgs::msg::String>("LalosoftDriveCommand", 10);
+
+    subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
+          "/joy", 10, std::bind(&RobotControlHost::joy_callback, this, _1));
+
+
     timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      500ms, std::bind(&RobotControlHost::timer_callback, this));
   }
 
 private:
@@ -42,15 +50,22 @@ private:
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
+
+  void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
+	{
+	  RCLCPP_INFO(this->get_logger(), "I heard: '%d'", msg->axes[0]);
+	}
+
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
   size_t count_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<RobotControlHost>());
   rclcpp::shutdown();
   return 0;
 }
