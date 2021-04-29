88d754ecfe8846ef0b23decac19f50af786d2eca
Casey Gregoire <caseyg@lalosoft.com>
2021-04-29 17:02:01 -0400

    updates to installer and implemented shared library correctly

---
+++ scripts/target_system/drive_module_rpi/opt/cotsbot/cotsbot_env_setup.sh
@@ -0,0 +1,9 @@
+/opt/cotsbot/cotsbot_env_vars.sh
+
+source /opt/ros/dashing/setup.bash
+
+echo "ROS2 setup, domain: $ROS_DOMAIN_ID"
+
+
+/opt/cotsbot/cotsbot_env_setup.sh
+
---
+++ scripts/target_system/drive_module_rpi/opt/cotsbot/cotsbot_env_vars.sh
@@ -0,0 +1,1 @@
+export ROS_DOMAIN_ID=2
